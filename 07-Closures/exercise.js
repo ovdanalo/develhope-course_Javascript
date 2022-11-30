345654a3a05562864fdd73dfa6d998bab644a906
Danilo Candida <candidad193@gmail.com>
2022-11-30 10:20:00 +0100

    add exercise about closures

---
+++ 07-Closures/exercise.js
@@ -0,0 +1,11 @@
+function printName() {
+    const helloName = 'Hello John';
+    function inner() {
+        setTimeout(() => {
+            console.log(helloName);
+        }, 1000);
+    }
+    return inner;
+}
+
+printName()()
