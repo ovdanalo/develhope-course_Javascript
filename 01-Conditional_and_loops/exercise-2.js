754b9f87addc2394661c5a2c324cf293beca89a2
Danilo Candida <candidad193@gmail.com>
2022-11-30 10:17:44 +0100

    add exercise about conditional and loops

---
+++ 01-Conditional_and_loops/exercise-2.js
@@ -0,0 +1,30 @@
+// ceo => 2200
+// manager => 1800
+// cto => 1800
+// developer => 1500
+// default => 1000
+function annualSalary(monthlySalary) {
+  return monthlySalary*13 + "€";
+}
+
+function calculateSalary(role) {
+  switch (role) {
+    case 'ceo': return annualSalary(2200);
+    case 'manager': return annualSalary(1800);
+    case 'cto': return annualSalary(1800);
+    case 'developer': return annualSalary(1500);
+    default: return annualSalary(1000);
+  }
+}
+
+const ceoSalary = calculateSalary('ceo');
+const managerSalary = calculateSalary('manager');
+const ctoSalary = calculateSalary('cto');
+const developerSalary = calculateSalary('developer');
+const otherSalary = calculateSalary('other');
+
+console.log(ceoSalary);
+console.log(managerSalary);
+console.log(ctoSalary);
+console.log(developerSalary);
+console.log(otherSalary);
