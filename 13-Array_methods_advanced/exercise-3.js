4eaea70d562c5cda9d173c7d7cebfd40bd382173
ovdanalo <104578306+ovdanalo@users.noreply.github.com>
2022-12-05 14:22:58 +0100

    add exercise about advanced array methods

---
+++ 13-Array_methods_advanced/exercise-3.js
@@ -0,0 +1,23 @@
+function calculateAverageAge(persons) {
+
+  let sum = persons.reduce((prevVal, currentVal) => prevVal + currentVal["age"], 0);
+  let average = Math.round(sum / 10);
+   return average;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
