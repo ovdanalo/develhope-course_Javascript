747ec65ec46e5e913cef46ccd861be0439770247
ovdanalo <104578306+ovdanalo@users.noreply.github.com>
2022-12-15 10:00:00 +0100

    add exercise about instanceof operator

---
+++ 20-Instanceof_operator/exercise-6.js
@@ -0,0 +1,38 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+  static calculate(shape) {
+    if (shape instanceof Square) {
+      return shape.side ** 2;
+    } else if (shape instanceof Rectangle) {
+      return shape.width * shape.height;
+    } else {
+      return Number((Math.PI * shape.radius ** 2).toFixed(2));
+    }
+  }
+}
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));
