41aa4ac8c3d0667f4652a1cd7e7b3d29434878e7
ovdanalo <104578306+ovdanalo@users.noreply.github.com>
2023-01-07 10:58:10 +0100

    fix exercise

--- 25-Rest_parameters_and_spread_syntax/exercise-3.js
+++ 25-Rest_parameters_and_spread_syntax/exercise-3.js
@@ -1,4 +1,7 @@
 let numberStore = [0, 1, 2];
 let newNumber = 3;
-numberStore = [...numberStore, newNumber];
+numberStore = [newNumber, ...numberStore];
 console.log(numberStore);
+
+//I now used the rest operator, but it isn't equivalent to the push, the push add the
+// newNumber at the end, the rest operator in this case is more similar to a shift
